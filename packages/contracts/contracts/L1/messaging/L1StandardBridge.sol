3fcc1b0422504d0b564c76f68ce6bfea9398da68
Maurelian <maurelian@protonmail.ch>
2021-10-09 13:33:52 -0400

    Update packages/contracts/contracts/L1/messaging/L1StandardBridge.sol

--- packages/contracts/contracts/L1/messaging/L1StandardBridge.sol
+++ packages/contracts/contracts/L1/messaging/L1StandardBridge.sol
@@ -88,7 +88,7 @@ contract L1StandardBridge is IL1StandardBridge, CrossDomainEnabled {
         _initiateETHDeposit(
             msg.sender,
             msg.sender,
-            100_000,
+            125_000,
             bytes("")
         );
     }
