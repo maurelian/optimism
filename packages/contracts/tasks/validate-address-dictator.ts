7e5c88d000a8c2bb5b5e62fe8af5e1c86ba038d8
Maurelian <maurelian@protonmail.ch>
2021-11-09 09:52:27 -0500

    feat(contracts): Add AddressDictator deployment validation script
    feat: Add task-utils, with interactive user prompt
    feat: Add colors to printed strings
    fix: Detect network name
    refactor: Extract comparison and logs into printComparison()
    feat: validate source code of all updated addresses
    fix: await isHardhatNode
    fix: Remove todo comments
    refactor: Extract to getContractArtifacts
    feat: Add validation of libAddressManager

---
+++ packages/contracts/tasks/validate-address-dictator.ts
@@ -0,0 +1,150 @@
+'use strict'
+
+import { ethers } from 'ethers'
+import { task } from 'hardhat/config'
+import * as types from 'hardhat/internal/core/params/argumentTypes'
+import { hexStringEquals } from '@eth-optimism/core-utils'
+import { getContractFactory } from '../src/contract-defs'
+
+import { getInput, color as c, getArtifact } from '../src/task-utils'
+
+const printComparison = (
+  action: string,
+  description: string,
+  value1: string,
+  value2: string
+) => {
+  console.log(action + ':')
+  if (hexStringEquals(value1, value2)) {
+    console.log(c.green(`${description} looks good! 😎`))
+  } else {
+    throw new Error(`${description} looks wrong`)
+  }
+  console.log() // Add some whitespace
+}
+
+task('validate:address-dictator')
+  // Provided by the signature Requestor
+  .addParam(
+    'dictator',
+    'Address of the AddressDictator to validate.',
+    undefined,
+    types.string
+  )
+  .addParam(
+    'manager',
+    'Address of the Address Manager contract which would be updated by the Dictator.',
+    undefined,
+    types.string
+  )
+  // Provided by the signers themselves.
+  .addParam(
+    'multisig',
+    'Address of the multisig contract which should be the final owner',
+    undefined,
+    types.string
+  )
+  .addOptionalParam(
+    'contractsRpcUrl',
+    'RPC Endpoint to query for data',
+    process.env.CONTRACTS_RPC_URL,
+    types.string
+  )
+  .setAction(async (args) => {
+    if (!process.env.CONTRACTS_RPC_URL) {
+      throw new Error(c.red('CONTRACTS_RPC_URL not set in your env.'))
+    }
+    const provider = new ethers.providers.JsonRpcProvider(args.contractsRpcUrl)
+
+    const network = await provider.getNetwork()
+    console.log(
+      `
+Validating the deployment on the chain with:
+Name: ${network.name}
+Chain ID: ${network.chainId}`
+    )
+    const res = await getInput(c.yellow('Does that look right? (Y/n)\n> '))
+    if (res !== 'Y') {
+      throw new Error(
+        c.red('User indicated that validation was run against the wrong chain')
+      )
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const dictatorArtifact = require('../artifacts/contracts/L1/deployment/AddressDictator.sol/AddressDictator.json')
+    const dictatorCode = await provider.getCode(args.dictator)
+    printComparison(
+      'Verifying AddressDictator source code against local build artifacts',
+      'Deployed AddressDictator code',
+      dictatorArtifact.deployedBytecode,
+      dictatorCode
+    )
+
+    // connect to the deployed AddressDictator
+    const dictatorContract = getContractFactory('AddressDictator')
+      .attach(args.dictator)
+      .connect(provider)
+
+    const finalOwner = await dictatorContract.finalOwner()
+    printComparison(
+      'Validating that finalOwner address in the AddressDictator matches multisig address',
+      'finalOwner',
+      finalOwner,
+      args.multisig
+    )
+
+    const manager = await dictatorContract.manager()
+    printComparison(
+      'Validating the AddressManager address in the AddressDictator',
+      'addressManager',
+      manager,
+      args.manager
+    )
+
+    // Get names and addresses from the Dictator.
+    const namedAddresses = await dictatorContract.getNamedAddresses()
+
+    // Connect to the deployed AddressManager so we can see which are changed or unchanged.
+    const managerContract = getContractFactory('Lib_AddressManager')
+      .attach(args.manager)
+      .connect(provider)
+
+    // Loop over those and compare the addresses/deployedBytecode to deployment artifacts.
+    for (const pair of namedAddresses) {
+      // Check for addresses that will not be changed:
+      const currentAddress = await managerContract.getAddress(pair.name)
+      const addressChanged = !hexStringEquals(currentAddress, pair.addr)
+      const artifact = getArtifact(pair.name)
+
+      if (addressChanged) {
+        console.log(`${pair.name} address will be updated.`)
+        console.log(`Before ${currentAddress}`)
+        console.log(`After ${pair.addr}`)
+
+        const code = await provider.getCode(pair.addr)
+        printComparison(
+          `Verifying ${pair.name} source code against local deployment artifacts`,
+          `Deployed ${pair.name} code`,
+          artifact.deployedBytecode,
+          code
+        )
+      } else {
+        console.log(`${pair.name} not updated`)
+      }
+      if (Object.keys(artifact)) {
+        if (artifact.abi.some((el) => el.name === 'libAddressManager')) {
+          const libAddressManager = await getContractFactory(pair.name)
+            .attach(pair.addr)
+            .connect(provider)
+            .libAddressManager()
+
+          printComparison(
+            `Verifying ${pair.name} has the correct AddressManager address`,
+            `The AddressManager address in ${pair.name}`,
+            libAddressManager,
+            manager
+          )
+        }
+      }
+    }
+  })
