603891cc0ef87bce5f6c2e5ac82923d7bf84e572
Mark Tyneway <mark.tyneway@gmail.com>
2022-04-14 16:05:24 -0700

    slither: fix

--- packages/contracts/.solcover.js
+++ packages/contracts/.solcover.js
@@ -2,10 +2,12 @@ module.exports = {
   skipFiles: [
     './test-helpers',
     './test-libraries',
-    './L2/predeploys/OVM_DeployerWhitelist.sol'
+    './L2/predeploys/OVM_DeployerWhitelist.sol',
+    './lib',
+    './L1/rollup/L2OutputOracle.sol',
   ],
   mocha: {
-    grep: "@skip-on-coverage",
-    invert: true
-  }
-};
+    grep: '@skip-on-coverage',
+    invert: true,
+  },
+}
